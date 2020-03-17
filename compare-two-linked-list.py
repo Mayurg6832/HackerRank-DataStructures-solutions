82ad0eb836e877e2d973e20445b9d229090e48d6
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-03-17 22:27:09 +0530

    Create compare-two-linked-list.py

---
+++ compare-two-linked-list.py
@@ -0,0 +1,93 @@
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    if llist1_count!=llist2_count:
+        return 0
+    else:
+        current1=llist1
+        current2=llist2
+        flag=0
+        while True:
+            if current1==None and current2==None:
+                break
+            elif current1.data==current2.data:
+                flag=1
+            else:
+                flag=0
+            current1=current1.next
+            current2=current2.next
+        if flag==1:
+            return 1
+        else:
+            return 0
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
